0f02b4508e5b050684a237f8bf3359a3e0ca851f
Matt Ragone <m.ragone85@gmail.com>
2020-06-25 22:31:24 -0400

    updated js

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # weatherApp
 
-A simple weather app using HTML, CSS and vanilla JS and the DarkSky.net API
+A simple weather app using HTML, CSS and vanilla JS and the OpenWeather API
--- app.js
+++ app.js
@@ -1,13 +1,63 @@
-window.addEventListener("load", () => {
-  let long;
-  let lat;
+const api = {
+  key: "70b123e5b92b2cb65a74ff88dc650610",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-    });
-  } else {
-    h1.textContent = "Please allow geolocation for app to work properly";
+const searchBox = document.querySelector(".search-box");
+searchBox.addEventListener("keypress", setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchBox.value);
   }
-});
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather) {
+  console.log(weather);
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
+}
+
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
